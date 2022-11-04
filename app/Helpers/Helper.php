2f309872df311e06db11ccd44f009d3e59229ef3
christmex <christmex@yahoo.com>
2022-11-04 07:27:38 +0700

    Update helper and db_mapping

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -13,24 +13,44 @@ class Helper {
 
     public static function Months(){
         return [
-            '1' => 'Januari',
-            '2' => 'Februari',
-            '3' => 'Maret',
-            '4' => 'April',
-            '5' => 'Mei',
-            '6' => 'Juni',
-            '7' => 'Juli',
-            '8' => 'Agustus',
-            '9' => 'September',
-            '10' => 'Oktober',
-            '11' => 'November',
-            '12' => 'Desember'
+            1 => 'Januari',
+            2 => 'Februari',
+            3 => 'Maret',
+            4 => 'April',
+            5 => 'Mei',
+            6 => 'Juni',
+            7 => 'Juli',
+            8 => 'Agustus',
+            9 => 'September',
+            10 => 'Oktober',
+            11 => 'November',
+            12 => 'Desember'
         ];
     }
 
+    public static function SchoolYearMonth(){
+        return [
+            1 => 'Juli',
+            2 => 'Agustus',
+            3 => 'September',
+            4 => 'Oktober',
+            5 => 'November',
+            6 => 'Desember',
+            7 => 'Januari',
+            8 => 'Februari',
+            9 => 'Maret',
+            10 => 'April',
+            11 => 'Mei',
+            12 => 'Juni',
+        ];
+    }
 
     public static function getMonthById($id){
         return self::Months()[$id];
+    }
+
+    public static function getSchoolYearMonthById($id){
+        return self::SchoolYearMonth()[$id];
     }
 
     public static function moneyFormat($value){
