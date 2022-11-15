3a4ce608c9daa936120a8ebddedd8dbca068a680
christmex <christmex@yahoo.com>
2022-11-15 08:32:53 +0700

    Adding filter cash in and out

--- app/Http/Livewire/PettyCash/Index.php
+++ app/Http/Livewire/PettyCash/Index.php
@@ -8,9 +8,12 @@ class Index extends Component
 {
 
     public $crud;
+    public $crudLoop;
     public $filter_start_date;
     public $filter_end_date;
     public $search;
+    public $filter_in_out;
+    public $query;
     
     protected $rules = [
         'filter_start_date' => 'date|before_or_equal:filter_end_date',
@@ -20,11 +23,24 @@ class Index extends Component
     public function mount($crud){
         $this->crud = $crud->model;
         $this->filter_start_date = $this->filter_end_date = date('Y-m-d');
+        $this->filter_debit_credit = 'ALL';
+
+        $this->crudLoop = $crud->model->whereBetween('trx_date',[$this->filter_start_date, $this->filter_end_date])->where('petty_cash_title', 'like', '%'.$this->search.'%')->get();
     }
 
     public function updated($propertyName)
     {
         $this->validateOnly($propertyName);
+
+        if($this->filter_debit_credit == 'ALL'){
+            $this->crudLoop = $this->crud->whereBetween('trx_date',[$this->filter_start_date, $this->filter_end_date])->where('petty_cash_title', 'like', '%'.$this->search.'%')->get();
+        }
+        if($this->filter_debit_credit == 'DEBIT'){
+            $this->crudLoop = $this->crud->whereBetween('trx_date',[$this->filter_start_date, $this->filter_end_date])->where('petty_cash_title', 'like', '%'.$this->search.'%')->where('debit','!=',0)->get();
+        }
+        if($this->filter_debit_credit == 'CREDIT'){
+            $this->crudLoop = $this->crud->whereBetween('trx_date',[$this->filter_start_date, $this->filter_end_date])->where('petty_cash_title', 'like', '%'.$this->search.'%')->where('credit','!=',0)->get();
+        }
     }
     
 
--- resources/views/livewire/petty-cash/index.blade.php
+++ resources/views/livewire/petty-cash/index.blade.php
@@ -50,27 +50,57 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="card">
-                <div class="card-header"><i class="fa fa-align-justify"></i> Detail Invoice</div>
+                <!-- <div class="card-header"><i class="fa fa-align-justify"></i> Detail Invoice</div> -->
                 <div class="card-body">
+                    <div class="row mb-3">
+                        <div class="col-sm-5">
+                            <h4 class="card-title mb-0">Petty Cash</h4>
+                            <div class="small text-muted">November 2022</div>
+                        </div>
+                        <!-- /.col-->
+                        <div class="col-sm-7 d-none d-md-block" wire:ignore>
+                            <div class="btn-group btn-group-toggle float-right mr-3" data-toggle="buttons">
+                            <label class="btn btn-outline-secondary">
+                                <input id="option1" type="radio" name="filter_debit_credit" wire:click="$set('filter_debit_credit', 'ALL')" autocomplete="off" checked=""> ALL
+                            </label>
+                            <label class="btn btn-outline-secondary">
+                                <input id="option2" type="radio" name="filter_debit_credit" wire:click="$set('filter_debit_credit', 'DEBIT')" autocomplete="off"> DEBIT
+                            </label>
+                            <label class="btn btn-outline-secondary">
+                                <input id="option3" type="radio" name="filter_debit_credit" wire:click="$set('filter_debit_credit', 'CREDIT')" autocomplete="off"> CREDIT
+                            </label>
+                            </div>
+                        </div>
+                        <!-- /.col-->
+                    </div>
+                    
                     <table class="table table-responsive-sm table-bordered table-striped table-hover table-sm">
                         <thead>
                         <tr>
                             <th>Petty Cash Title</th>
-                            <th>Debit</th>
-                            <th>Credit</th>
+                            @if($filter_debit_credit == 'ALL' || $filter_debit_credit == 'DEBIT')
+                                <th>Debit</th>
+                            @endif 
+                            @if($filter_debit_credit == 'ALL' || $filter_debit_credit == 'CREDIT')
+                                <th>Credit</th>
+                            @endif
                             <th>TRX Date</th>
                         </tr>
                         </thead>
                         <tbody>
-                            @foreach($crud->whereBetween('trx_date',[$filter_start_date, $filter_end_date])->where('petty_cash_title', 'like', '%'.$search.'%')->get() as $data)
+                            @foreach($crudLoop as $data)
                                 <tr>
                                     <td>
                                         {!! $data->petty_cash_title !!}
                                     </td>
-                                    <td>
-                                        {{$data->debitMoneyFormat}}
-                                    </td>
-                                    <td>{{$data->creditMoneyFormat}}</td>
+                                    @if($filter_debit_credit == 'ALL' || $filter_debit_credit == 'DEBIT')
+                                        <td>
+                                            {{$data->debitMoneyFormat}}
+                                        </td>
+                                    @endif
+                                    @if($filter_debit_credit == 'ALL' || $filter_debit_credit == 'CREDIT')
+                                        <td>{{$data->creditMoneyFormat}}</td>
+                                    @endif
                                     <td>{{$data->trx_date}}</td>
                                 </tr>
                             @endforeach
@@ -78,9 +108,15 @@
                         <tfoot>
                             <tr>
                                 <th>Total</th>
-                                <th>{{Helper::MoneyFormat($crud->sumDebit($filter_start_date,$filter_end_date,$search))}}</th>
-                                <th>{{Helper::MoneyFormat($crud->sumCredit($filter_start_date,$filter_end_date,$search))}}</th>
-                                <th>{{$crud->sumCreditAndDebitMoneyFormat($filter_start_date,$filter_end_date,$search)}}</th>
+                                @if($filter_debit_credit == 'ALL' || $filter_debit_credit == 'DEBIT')
+                                    <th>{{Helper::MoneyFormat($crud->sumDebit($filter_start_date,$filter_end_date,$search))}}</th>
+                                @endif
+                                @if($filter_debit_credit == 'ALL' || $filter_debit_credit == 'CREDIT')
+                                    <th>{{Helper::MoneyFormat($crud->sumCredit($filter_start_date,$filter_end_date,$search))}}</th>
+                                @endif
+                                @if($filter_debit_credit == 'ALL')
+                                    <th>{{$crud->sumCreditAndDebitMoneyFormat($filter_start_date,$filter_end_date,$search)}}</th>
+                                @endif
                             </tr>
                         </tfoot>
                     </table>
